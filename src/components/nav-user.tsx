f19837ca993ef63a3739258321597e316872ade5
haziq <haziq.musa02@gmail.com>
2025-02-03 15:40:08 +0800

    update

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -32,7 +32,9 @@ export function NavUser({ user }: { user?: NavUserProps }) {
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user?.avatar} alt={user?.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">
+                  {user?.name.slice(0, 2)}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user?.name}</span>
