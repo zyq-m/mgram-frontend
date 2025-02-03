7e78611117b414f9e242d78e10c3123660430a9f
haziq <haziq.musa02@gmail.com>
2025-02-03 16:19:16 +0800

    protected route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,9 @@ import Predict from "./predict/page";
 import Result from "./result/page";
 import ResultDetails from "./result/details";
 import AdminDashboard from "./admin/dashboard";
+import { Navigate, Outlet } from "react-router";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export default {
   LoginPage,
@@ -13,3 +16,43 @@ export default {
   ResultDetails,
   AdminDashboard,
 };
+
+type ProtectedRouteProps = {
+  redirect?: string;
+  allowed: string[];
+};
+
+type AuthUser = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+};
+
+export function ProtectedRoute({
+  redirect = "/app/home",
+  allowed,
+}: ProtectedRouteProps) {
+  const token = sessionStorage.getItem("accessToken");
+  const [auth, setAuth] = useState<AuthUser | null>(null);
+
+  useEffect(() => {
+    if (token) {
+      const { sub }: { sub: AuthUser } = jwtDecode(token);
+      setAuth(sub);
+    }
+  }, [token]);
+
+  if (!auth) {
+    // Render null or a loading component until the auth state is set
+    return null;
+  }
+
+  if (!allowed.includes(auth?.role)) {
+    return <Navigate to={redirect} replace />;
+  }
+
+  console.log(!allowed.includes(auth?.role));
+
+  return <Outlet />;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./app";
+import App, { ProtectedRoute } from "./app";
 import RootLayout from "./components/layout/layout";
 
 createRoot(document.getElementById("root")!).render(
@@ -11,12 +11,25 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/app" element={<App.LoginPage />} />
         <Route element={<RootLayout />}>
-          <Route path="app/home" element={<App.Dashboard />} />
-          <Route path="app/predict" element={<App.Predict />} />
-          <Route path="app/result" element={<App.Result />} />
-          <Route path="app/result/:id" element={<App.ResultDetails />} />
-
-          <Route path="app/admin/home" element={<App.AdminDashboard />} />
+          <Route
+            element={
+              <ProtectedRoute allowed={["USER"]} redirect="/app/admin/home" />
+            }
+          >
+            <Route path="app/home" element={<App.Dashboard />} />
+            <Route path="app/predict" element={<App.Predict />} />
+          </Route>
+          <Route element={<ProtectedRoute allowed={["USER", "ADMIN"]} />}>
+            <Route path="app/result" element={<App.Result />} />
+            <Route path="app/result/:id" element={<App.ResultDetails />} />
+          </Route>
+          <Route
+            element={
+              <ProtectedRoute allowed={["ADMIN"]} redirect="/app/home" />
+            }
+          >
+            <Route path="app/admin/home" element={<App.AdminDashboard />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
